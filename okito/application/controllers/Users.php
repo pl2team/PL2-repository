16fba2c96c93ef069c42e9d990cee19d838c6f30
Misi <mpekar55@gmail.com>
2019-05-16 21:13:57 +0200

    bootstrap hozzáadása és registration veiw alap létrehozása

--- okito/application/controllers/Users.php
+++ okito/application/controllers/Users.php
@@ -3,6 +3,7 @@
 class Users extends CI_Controller{
     
     public function Registration(){
+        $this->load->helper('url');
         $this->load->view('registration');
     }
     public function register(){
--- okito/application/views/registration.php
+++ okito/application/views/registration.php
@@ -1,3 +1,11 @@
-<?php
-
-echo 'hello_world';
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css">
+    <title>Regisztráció</title>
+    </head>
+    <body>
+        <p class="btn btn-primary">This is a button</p> 
+    </body>
+</html>
