00f76d34b56bebb3a29434552cd6da0b11f6b188
Misi <mpekar55@gmail.com>
2019-05-16 21:33:04 +0200

    Form hozzáadása a viewhoz és helperek betöltése(form,url)

--- okito/application/controllers/Users.php
+++ okito/application/controllers/Users.php
@@ -4,6 +4,7 @@ class Users extends CI_Controller{
     
     public function Registration(){
         $this->load->helper('url');
+        $this->load->helper('form');
         $this->load->view('registration');
     }
     public function register(){
--- okito/application/views/registration.php
+++ okito/application/views/registration.php
@@ -6,6 +6,26 @@
     <title>Regisztráció</title>
     </head>
     <body>
-        <p class="btn btn-primary">This is a button</p> 
+        <div class="container">
+            <h1>Regisztráció</h1>
+            <?php echo form_open('users/register');?>
+                <div>                   
+                    <div class="form-group">
+                       <label>Teljes neved:</label>
+                       <input type="text" class="form-control" name="name" placeholder="Név megadása.">
+                       <label>Felhasználónév:</label>
+                       <input type="text" class="form-control" name="username" placeholder="Felhasználónév megadása.">
+                       <label>E-mail: </label>
+                       <input type="email" class="form-control" name="email" placeholder="E-mail cím megadása">
+                       <label>Jelszó:</label>
+                       <input type="password" class="form-control" name="password" placeholder=" Jelszó megadása.">
+                       <label>Jelszó megerősítése:</label>
+                       <input type="password" class="form-control" name="password2" placeholder="Jelszó megadása."> 
+                    <br>
+                <button type="submit" class ="btn btn-primary ">Regisztráció</button>
+                </div>
+            </div>
+            <?php echo form_close();?>
+        </div>
     </body>
 </html>
