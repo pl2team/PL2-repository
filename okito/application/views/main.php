21a6928a7840515b141e257c529240cc8fe812d6
Orsolya Takacsne Mihok <orsolya@takacs.eu>
2019-05-26 15:56:12 +0200

    Főoldal (tantárgyak) view keretének létrehozása

---
+++ okito/application/views/main.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
+    <title>Tantárgyak</title>
+    </head>
+    <body class="regbody">
+        <div  class="container h-100">
+            <div class="d-flex justify-content-center h-100">
+            <div  style="margin-top:20%;margin-bottom:20%; width:60%">
+                <div class=" user_card ">
+                    <h1 style="padding: 0 !important" class="title">Tantárgyak</h1>
+                    <div>
+                       Tantárgy 1
+                        Tantárgy2
+
+                    </div>
+                </div>
+                </div>
+            </div>                        
+        </div>
+    </body>
+</html>
