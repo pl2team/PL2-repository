3f1419ae9913d3c05b2a758c254c0e035eb038d0
Misi <mpekar55@gmail.com>
2019-05-26 21:11:46 +0200

    StudyMaterials route hozzáadása a route confighoz

--- okito/application/config/routes.php
+++ okito/application/config/routes.php
@@ -54,5 +54,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['registration']="users/registration";
 $route['default_controller'] = 'users/login';
 $route['courses'] = 'Courses';
+$route['studymaterials'] = 'StudyMaterials';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
