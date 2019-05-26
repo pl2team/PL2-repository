13e36af9d12b41de43c55699f216b2dfacae3d7f
Orsolya Takacsne Mihok <orsolya@takacs.eu>
2019-05-26 17:54:12 +0200

    Főoldal (tantárgyak) view és css elkészítése

--- okito/application/views/main.php
+++ okito/application/views/main.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html>
-    <head>
+<head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
     <title>Tantárgyak</title>
-    </head>
-    <body class="regbody">
-        <div  class="container h-100">
-            <div class="d-flex justify-content-center h-100">
-            <div  style="margin-top:20%;margin-bottom:20%; width:60%">
-                <div class=" user_card ">
-                    <h1 style="padding: 0 !important" class="title">Tantárgyak</h1>
-                    <div>
-                       Tantárgy 1
-                        Tantárgy2
-
+</head>
+<body class="coursebody">
+<div class="container h-100">
+    <div class="d-flex justify-content-center h-100">
+        <div style="margin-top:20%;margin-bottom:20%; width:60%">
+            <?php foreach ($courses as $course) { ?>
+                <div class=" element_card ">
+                    <h1 style="padding: 0 !important" class="title"><?php echo $course->Megnevezes; ?></h1>
+                    <div class="d-flex justify-content-center " style="color:#325D88;">
+                        (<?php echo $course->Kategoria; ?>)
                     </div>
                 </div>
-                </div>
-            </div>                        
+            <?php } ?>
         </div>
-    </body>
+    </div>
+</div>
+</body>
 </html>
