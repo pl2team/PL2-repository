7b1244651d2642d43f8fc1749c7a04b1581bb7a5
Belincsák György <belincsakgyorgy@gmail.com>
2019-05-26 21:56:34 +0200

    study_materials_model létrehozása és StuyMaterialsGet function létrehozása

---
+++ okito/application/models/study_materials_model.php
@@ -0,0 +1,15 @@
+<?php
+
+class study_materials_model extends CI_Model
+{
+    public function __construct()
+   {
+        $this->load->database();
+    }
+
+    function StudyMaterialsGet($IdTantargy)
+    {
+        $query = $this->db->get_where('tananyagok', array('IdTantargy' => $IdTantargy));
+        return $query->result();
+    }
+}
