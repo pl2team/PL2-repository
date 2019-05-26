0b518c04d7043193ef3df40ef7aa9509c6ed25da
Orsolya Takacsne Mihok <orsolya@takacs.eu>
2019-05-26 18:44:01 +0200

    Főoldal (tantárgyak) tantárgy kattintható

--- okito/application/views/main.php
+++ okito/application/views/main.php
@@ -11,12 +11,14 @@
     <div class="d-flex justify-content-center h-100">
         <div style="margin-top:20%;margin-bottom:20%; width:60%">
             <?php foreach ($courses as $course) { ?>
-                <div class=" element_card ">
-                    <h1 style="padding: 0 !important" class="title"><?php echo $course->Megnevezes; ?></h1>
-                    <div class="d-flex justify-content-center " style="color:#325D88;">
-                        (<?php echo $course->Kategoria; ?>)
+                <a href="course?id=<?php echo $course->IdTantargyak; ?>">
+                    <div class=" element_card ">
+                        <h1 style="padding: 0 !important" class="title"><?php echo $course->Megnevezes; ?></h1>
+                        <div class="d-flex justify-content-center " style="color:#325D88;">
+                            (<?php echo $course->Kategoria; ?>)
+                        </div>
                     </div>
-                </div>
+                </a>
             <?php } ?>
         </div>
     </div>
