9cc8221e9d319a3570928f83a5de39df30c7277d
andoren <mpekar55@gmail.com>
2019-06-08 16:26:59 +0200

    courses menü pont hozzáadása a menü szerkezethez

--- okito/application/views/templates/header.php
+++ okito/application/views/templates/header.php
@@ -34,16 +34,18 @@
           <a class="dropdown-item" href="#">Törlés</a>
         </div>
       </li>
-            <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Courses
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Hozzáadás</a>
+          <a class="dropdown-item" href="#">Módosítás</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Törlés</a>
         </div>
       </li>
+
       <li class="nav-item">
         <a class="nav-link disabled" href="#">Felhasználó kezelés</a>
       </li>
