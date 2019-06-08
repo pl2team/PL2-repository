698185cb54a6d52cc96d851bb5dbe93e5802196d
andoren <mpekar55@gmail.com>
2019-06-08 21:00:32 +0200

    Tananyag create function létrehozása a StudyMaterial controllerben

--- okito/application/models/study_materials_model.php
+++ okito/application/models/study_materials_model.php
@@ -5,6 +5,8 @@ class study_materials_model extends CI_Model
     public function __construct()
    {
         $this->load->database();
+                $this->load->helper('url');
+        $this->load->helper('form');
     }
 
     function StudyMaterialsGet($IdTantargy)
@@ -12,5 +14,24 @@ class study_materials_model extends CI_Model
         $query = $this->db->get_where('tananyagok', array('IdTantargy' => $IdTantargy));
         return $query->result();
     }
-
+    public function create(){
+        if(!$this->session->userdata('logged_in')){
+           redirect('login');
+       }
+        $data['title']='Tananyag létrehozása';
+        $this->form_validation->set_rules('title','Cím','required');
+        $this->form_validation->set_rules('body','Szöveg','required');
+        if($this->form_validation->run()===FALSE){
+            $this->load->view('templates/header');
+            $this->load->view('createlesson',$data);
+            $this->load->view('templates/footer');           
+        }
+        else{
+            $this->load->model('study_materials_model');
+            $this->page_model->create_lesson();   
+            $this->log_model->log('Page created',$this->session->userdata('user_id'));
+            $this->session->set_flashdata('created','Sikeresen hozzáadtad a tananyagot');
+            redirect('courses');  
+        }
+    }
 }
