a851dcba4b26db3b7df2d31ddb979500a67245d4
Belincsák György <belincsakgyorgy@gmail.com>
2019-06-08 14:29:09 +0200

    Template header fájl megírása

---
+++ okito/application/views/templates/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">   
+</head>
+<body>
