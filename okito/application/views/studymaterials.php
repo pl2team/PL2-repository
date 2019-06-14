8e42a8713d00a0d759fdaabd474850da4daf63ad
Renyhárt Gábor <renyhart.gabor@gmail.com>
2019-06-14 23:05:14 +0200

    A tartalom most már feljebb kezdődik (-5%)

--- okito/application/views/studymaterials.php
+++ okito/application/views/studymaterials.php
@@ -3,7 +3,7 @@
 <a href="<?PHP echo base_url()?>Courses"><img class="brand_logo2" src="<?php echo base_url()?>/assets/images/cica2.png" class="cica_logo" alt="Logo"></a>
 <div class="container h-100">
     <div class="d-flex justify-content-center h-100">
-        <div style="margin-top:20%;margin-bottom:20%; width:60%">
+        <div style="margin-top:-5%;margin-bottom:20%; width:60%">
             <?php foreach ($studyMaterials as $studyMaterial) { ?>
             <a href="<?php echo base_url(); ?>studymaterials/lesson/<?php echo $studyMaterial['IdTananyag']; ?>">
                     <div class=" element_card ">
